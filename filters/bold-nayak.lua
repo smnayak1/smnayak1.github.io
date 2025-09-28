fb0e6d29f9e5b4d250e7f18c63034b12341baaf3
sandeep <smnayak1@gmail.com>
2025-09-27 21:27:00 -0400

    qwd

--- filters/bold-nayak.lua
+++
@@ -1,9 +0,0 @@
--- Bold "Sandeep M. Nayak" in the rendered reference list
-function Div(el)
-  if el.identifier == "refs" then
-    local html = pandoc.write(pandoc.Pandoc(el.content), "html")
-    html = html:gsub("Sandeep M%.?%s*Nayak", "<strong>Sandeep M. Nayak</strong>")
-    html = html:gsub("Nayak, S%.?%s*M%.?", "<strong>Nayak, S. M.</strong>")
-    return pandoc.RawBlock("html", html)
-  end
-end
